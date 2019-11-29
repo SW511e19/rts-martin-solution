68f5ad8f0bda4f3649f17eac21fa628291226f85
Martin <36505224+OlympicalStoner@users.noreply.github.com>
2019-11-29 13:54:55 +0100

    Gjort koden renere.

--- deadlinetest.py
+++ deadlinetest.py
@@ -3,10 +3,24 @@ from ev3dev2.motor import LargeMotor, MediumMotor, OUTPUT_B, OUTPUT_C, SpeedPerc
 import time
 import datetime as dt
 
-m1 = MediumMotor(OUTPUT_B)
-#m2 = LargeMotor(OUTPUT_C)
+#m1 = MediumMotor(OUTPUT_B)
+m2 = LargeMotor(OUTPUT_C)
 #m1.on(SpeedPercent(0))
 #m1.on_for_rotations(SpeedPercent(0), 1)
+
+def best(array):
+    a = array[0]
+    for x in array:
+        if x < a:
+            a = x
+    return a
+
+def worst(array):
+    c = array[0]
+    for x in array:
+        if x > c:
+            c = x
+    return c
 
 def individual_motortest(motor, speed):
     count = 0
@@ -20,15 +34,24 @@ def individual_motortest(motor, speed):
         times.append(y)
         count += 1
         time.sleep(0.05)
-    a = times[0]
-    for x in times:
-        if x < a:
-            a = x
-    print("Best: " + str(a))
-    c = 0
-    for x in times:
-        if x > c:
-            c = x
-    print("Worst: " + str(c))
+    print("Best: " + str(best(times)))
+    print("Worst: " + str(worst(times)))
 
-individual_motortest(m1, -10)
+def manual_test(motor, speed):
+    count = 0
+    times = []
+    while count < 10:
+        input("Awaiting start")
+        x = dt.datetime.now().timestamp() * 1000
+        motor.on(SpeedPercent(speed))
+        input("Awaiting stop")
+        motor.on(SpeedPercent(0))
+        y = dt.datetime.now().timestamp() * 1000 - x
+        print(y)
+        times.append(y)
+        count += 1
+    print("Best: " + str(best(times)))
+    print("Worst: " + str(worst(times)))
+
+manual_test(m2, -30)
+#individual_motortest(m1, -10)
