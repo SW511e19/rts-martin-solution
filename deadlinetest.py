922c6a0f03a368d48d734d09db80fe68d1168209
Martin <36505224+OlympicalStoner@users.noreply.github.com>
2019-11-29 11:15:19 +0100

    Updated deadlinetest to have bestcase instead of average

--- deadlinetest.py
+++ deadlinetest.py
@@ -3,7 +3,7 @@ from ev3dev2.motor import LargeMotor, MediumMotor, OUTPUT_B, OUTPUT_C, SpeedPerc
 import time
 import datetime as dt
 
-m1 = LargeMotor(OUTPUT_B)
+m1 = MediumMotor(OUTPUT_B)
 #m2 = LargeMotor(OUTPUT_C)
 #m1.on(SpeedPercent(0))
 #m1.on_for_rotations(SpeedPercent(0), 1)
@@ -16,17 +16,19 @@ def individual_motortest(motor, speed):
         motor.on_for_rotations(SpeedPercent(speed), 1)
         #motor.on(SpeedPercent(0))
         y = dt.datetime.now().timestamp() * 1000 - x
+        print(y)
         times.append(y)
         count += 1
-    a = 0
+        time.sleep(0.05)
+    a = times[0]
     for x in times:
-        a += x
-    average = a / len(times)
-    print(str(average))
+        if x < a:
+            a = x
+    print("Best: " + str(a))
     c = 0
     for x in times:
         if x > c:
             c = x
-    print(str(c))
+    print("Worst: " + str(c))
 
-individual_motortest(m1, -100)
+individual_motortest(m1, -10)
