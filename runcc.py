996977c1b15cb1443af69579b51a2bf95b4e716f
Huntersz <37700694+Cklein17@users.noreply.github.com>
2019-12-06 11:29:38 +0100

    Update runcc.py

--- runcc.py
+++ runcc.py
@@ -9,69 +9,67 @@ m2 = LargeMotor(OUTPUT_C) # Cord has to go under the wheel
 
 global_speed = 25 # positive is left, negative is right
 
-# Runs the motors for a small amount of time before stopping
-# m2 runs slower to compensate for higher physical motor speed
+# Runs the motors. Which motor starts first depends on the direction. 
+# The motor not pulling the CC starts first to loosen the string a little.
 def run(speed):
     if speed >= 0:
-        m2.on(SpeedPercent(speed * 1.3))
+        m2.on(SpeedPercent(speed * 1.5))
         m1.on(SpeedPercent(speed))
     else:
         m1.on(SpeedPercent(speed * 1.3))
         m2.on(SpeedPercent(speed))
 
+# Tightens the string. Which motor starts first depends on the direction. 
 def tighten(speed, seconds):
     print("tightens")
     if speed >= 0:
-        m2.on(SpeedPercent(-(speed)))
+        m2.on(SpeedPercent(-speed))
         m1.on(SpeedPercent(speed))
     else:
         m1.on(SpeedPercent(speed))
-        m2.on(SpeedPercent(-(speed)))
+        m2.on(SpeedPercent(-speed))
     time.sleep(seconds)
     stop_motor()
 
+# Stops the motors
 def stop_motor():
     m1.on(SpeedPercent(0))
     m2.on(SpeedPercent(0))
 
-# calibrates CC's start pos to just outside red
+# Brings the CC to the red line. Should only be called once at the start of the program.
 def calibrate():
-    string_reset = 0
+    iteration = 0
     while (colorsensor.color != colorsensor.COLOR_RED):
         run(25)       
-        if string_reset % 10 == 0:
+        if iteration % 10 == 0:
             tighten(20, 0.1)
-        string_reset += 1
-    stop_motor()
+        iteration += 1
+    tighten(20, 0.1)
 
-
-# To compensate for sensor getting false positive by reading a change away from white and back to white again despite not having moved much,
-# count a minimum amount of iterations that at least should have passed before looking for a new white. There will be a certain minimum distance
-# because there always are a few iterations when moving between each box
-# If the card collector just calibrated there is very little distance to the first box so count will be less than 50 when reaching it.
-
+# To compensate for color-sensor characteristics (such as changing color values without moving),
+#   a minimum amount of iterations are done before searching for white when moving,
+#   this is to prevent that the same white is counted twice when moving between each box.
+# If the card collector just calibrated it will read red, it will search for white immediately,
+#   because the first white is less than the minimum amount of iterations away from the red.
 def cc(box_number, box_offset, speed):
-    string_reset = 0
-    white_reset = 0
+    iteration = 0
     look_for_white = False
-
-    if colorsensor.color != colorsensor.COLOR_WHITE:
+    
+    if(colorsensor.color == colorsensor.COLOR_RED):
         look_for_white = True
 
     while box_number != box_offset:
-        run(speed)
-
-        if string_reset % 10 == 0:
+        run(speed)      
+        if iteration % 10 == 0 and iteration != 0:
             tighten(20, 0.1)
 
-        if white_reset >= 50:
+        if iteration >= 50:
             look_for_white = True
-            white_reset = 0
 
         if look_for_white == True and colorsensor.color == colorsensor.COLOR_WHITE:
-            stop_motor()
-            white_reset = 0
             look_for_white = False
+            iteration = 0
+            stop_motor()
             
             print("was at " + str(box_number))
             if speed >= 0:
@@ -79,25 +77,25 @@ def cc(box_number, box_offset, speed):
             else:
                 box_number += 1
             print("is at " + str(box_number))   
-        
-        string_reset += 1
-        white_reset += 1
+        iteration += 1
+    stop_motor()
     return box_number
 
-# Go to box 1 to 7. Which direction it goes depends on the current box position and which box it should go to
+# Go to box 1 to 7. Which direction depends on the current position and which direction it should move.
 def go_to_box(current_box, dest_box, speed):
-    if(dest_box == current_box):
-        print("Was already at box")
-    if dest_box < current_box:
-        current_box = cc(current_box, dest_box, speed)
+    if 0 <= current_box <= 7 and 1 <= dest_box <= 7:
+        if dest_box == current_box:
+            print("Was already at box")
+        if dest_box < current_box:
+            current_box = cc(current_box, dest_box, speed)
+        if dest_box > current_box:
+            current_box = cc(current_box, dest_box, -speed)
     else:
-        current_box = cc(current_box, dest_box, -(speed))
-    stop_motor()
+        print("Box value out of bounds")
     return current_box
 
-calibrate() # Goes to the red line at the end of the machine before any boxes, position 0
+calibrate() 
 print("done calibrating")
-time.sleep(5)
 box_num = 1
 current_box = 0
 num_to_add = 1
